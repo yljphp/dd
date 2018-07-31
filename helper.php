315310a954660af027b82157140008a4a78f6acd
杨立景 <yanglijing@mafengwo.com>
2018-07-31 12:19:23 +0800

    first commit

---
+++ helper.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Symfony\Component\VarDumper\VarDumper;
+
+if (!function_exists('dump')) {
+    /**
+     * @author Nicolas Grekas <p@tchwork.com>
+     */
+    function dump($var)
+    {
+        foreach (func_get_args() as $var) {
+            VarDumper::dump($var);
+        }
+
+        if (1 < func_num_args()) {
+            return func_get_args();
+        }
+
+        return $var;
+    }
+}
+
+if (! function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function dd(...$args)
+    {
+        foreach ($args as $x) {
+            dump($x);
+        }
+        die(1);
+    }
+}
